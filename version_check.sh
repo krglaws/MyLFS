82530b3de80d85d8f8c7855f3d1caece1579c9fb
Kyle Glaws <kyle.glaws@gmail.com>
2022-02-19 18:02:31 -0500

    add description to top of version_check.sh

--- version_check.sh
+++ version_check.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Script to check build dependencies
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This is likely to break on many machines.
+# If it does, just manually check that you have
+# the right dependencies and versions according
+# to the listing in LFS 11.0, Chapter 2.2.
+
 EXIT_STATUS=0
 
 function compare_version {
