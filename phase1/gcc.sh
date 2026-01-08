6cdccee1a33b2a6e7f62cf2094eaf2c7aed97b67
mokrunka <hunker.engineer@gmail.com>
2026-01-07 18:43:55 -0800

    fix a typo in the gcc source URL; update phase 1 gcc.sh

--- phase1/gcc.sh
+++ phase1/gcc.sh
@@ -48,5 +48,5 @@ make install
 cd ..
 
 cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-  $(dirname $($LFS_TGT-gcc -print-libgcc-file-name))/include/limits.h 
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h 
 
